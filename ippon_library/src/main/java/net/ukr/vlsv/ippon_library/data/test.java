7257a46b2f324d7d535083947149bd306fd50f8b
vvlasiuk <vlsv@ukr.net>
2019-02-08 21:35:37 +0200

    Initial commit

---
+++ ippon_library/src/main/java/net/ukr/vlsv/ippon_library/data/test.java
@@ -0,0 +1,4 @@
+package net.ukr.vlsv.ippon_library.data;
+
+public class test {
+}
---
+++ ippon_user/src/main/java/net/ukr/vlsv/ippon/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.ukr.vlsv.ippon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
